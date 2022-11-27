9283633a9107b55f5ea497c92bb03e82fea57f1b
Kokou KOUMAGNA <kokou.koumagnagottoh@gmail.com>
2022-11-27 16:08:00 +0100

    :tada: Begin a new modulith setup project.

---
+++ src/main/java/com/heyrudy/advancedwebapi/AdvancedWebApiApplication.java
@@ -0,0 +1,13 @@
+package com.heyrudy.advancedwebapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancedWebApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdvancedWebApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/heyrudy/advancedwebapi/AdvancedWebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heyrudy.advancedwebapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvancedWebApiApplicationTests {
+
+//    @Test
+//    void contextLoads() {
+//    }
+
+}
